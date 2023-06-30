0946d7e427688fac3f96a61495f8afdec6cb44f1
jrexiti <jay.rexiti@gmail.com>
2023-06-30 13:23:03 -0400

    added notes on test class

--- src/test/java/com/project1/OrderCompleteTest.java
+++ src/test/java/com/project1/OrderCompleteTest.java
@@ -12,11 +12,14 @@ import pageObjects.ShopHomePage;
 
 public class OrderCompleteTest extends BasePage {
 
+	// calls the BasePage Constructor
 	public OrderCompleteTest() throws IOException {
 		super();
 	}
 
 	@BeforeTest
+
+	// this driver is the driver coming from base page
 	public void setup() throws InterruptedException, IOException {
 		driver = getDriver();
 		driver.get(getUrl());
@@ -29,14 +32,17 @@ public class OrderCompleteTest extends BasePage {
 		driver = null;
 	}
 
+	// creating objects of pages to interact with the locator, again the driver
+	// passed in the constructor is coming from the base page
 	@Test
 	public void endToEndTest() {
 		HomePage home = new HomePage(driver);
 		home.getTestStoreLink().click();
-		
+
 		ShopHomePage shopHome = new ShopHomePage(driver);
-		
-		
+		shopHome.getProductEight().click();
+
+		/// you get the idea.......
 
 	}
 
